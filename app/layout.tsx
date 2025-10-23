fdc155dec8762b6bfb6ee5d6b20c5c90346471d0
Brylle <bryanpalay119@gmail.com>
2025-10-23 20:48:52 +0800

    main

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Quiz let",
+  title: "A+ Quiz",
   description: "A simple quiz application",
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
 
 export default function Home() {
     // this is the main landing page
+    return (
+        <div>
+            <h1>Welcome to the A+ Quiz System</h1>
+        </div>
+    );
 }
