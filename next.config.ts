640f04ea2ecc6d7a1312f8ab642baa5defe0a95c
Brylle <bryanpalay119@gmail.com>
2025-11-15 19:00:46 +0800

    Enhance Next.js configuration with experimental typedEnv support, add new image domains and remote patterns, and configure ESLint to ignore errors during production builds.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental:{
+    typedEnv: true,
+  },
   images: {
-    domains: ['ui-avatars.com'],
+    domains: ['ui-avatars.com', 'ik.imagekit.io'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'html.tailus.io',
+        pathname: '/**',
+      },
+    ],
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
   /* config options here */
 };
